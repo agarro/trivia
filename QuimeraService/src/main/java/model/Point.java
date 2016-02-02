5482161c7cd28c00b1f7d252d6d2f944978e5425
Pedro Bogado <bogadopedro@gmail.com>
2016-02-01 22:50:56 -0300

    QuimeraServer - Point class added

---
+++ QuimeraService/src/main/java/model/Point.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Created by Manu on 1/2/16.
+ */
+public class Point {
+
+    private Participant participant;
+    private int points;
+
+    public Participant getParticipant() {
+        return participant;
+    }
+
+    public void setParticipant(Participant participant) {
+        this.participant = participant;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
